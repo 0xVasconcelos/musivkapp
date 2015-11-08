e8bff4b369c9b17aa85096104dcef932bc14d1ec
lucaslg26 <lucaslg26@yahoo.com.br>
2015-11-08 20:29:46 -0300

    try fix

--- dist/js/musivk.js
+++ dist/js/musivk.js
@@ -4,7 +4,7 @@ $("#searchButton").click(function () {
         $('#searchOverlay').show();
         var songNameEnc = encodeURIComponent(searchValue);
         $.ajax({
-            url: 'http://107.170.7.193:2626/api/v1/searchSong/' + songNameEnc +'/8c150f55b11b192cb9b5f1183c59811f97f0445937ebea15f2f1380c6722f8dff0a51cb756173a94bfc92',
+            url: 'http://music.cpux.me:2626/api/v1/searchSong/' + songNameEnc +'/8c150f55b11b192cb9b5f1183c59811f97f0445937ebea15f2f1380c6722f8dff0a51cb756173a94bfc92',
             success: function (data) {
                 $('#searchArea').hide();
                 $('#headerArea').hide();
